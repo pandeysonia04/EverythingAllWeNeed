acd4fa37031318bb6753418fb2c0fe5d3a01d537
pandeysonia04 <sonypandey371@gmail.com>
2022-03-28 10:12:36 +0530

    Section 10 Completed

--- Backend Code/ECommBackendCode/src/main/java/com/example/demo/repo/ProductRepository.java
+++ Backend Code/ECommBackendCode/src/main/java/com/example/demo/repo/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.example.demo.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.demo.entity.Product;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
 }
